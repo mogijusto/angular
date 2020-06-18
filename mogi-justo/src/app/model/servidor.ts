052fdc09632dd09ff7197456f45a8c2713c023d3
gegeuKaue <53498208+gegeuKaue@users.noreply.github.com>
2020-06-18 19:47:47 -0300

    prova

--- mogi-justo/src/app/model/servidor.ts
+++ mogi-justo/src/app/model/servidor.ts
@@ -4,4 +4,13 @@ export class Servidor {
     nome: string;
     cargo: string;
     rendimentos: number;
+
+    static anyToServidor(servidor: any): Servidor {
+        const servidorTemp = new Servidor();
+        servidorTemp.cargo = servidor.cargo;
+        servidorTemp.nome = servidor.nome;
+        servidorTemp.rendimentos = +servidor.rendimentos.replace('.', '').replace(',', '.');
+        servidorTemp.rgf = +servidor.rgf;
+        return servidorTemp;
+    }
 }
